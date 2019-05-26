cc54200d9cb6a618a8dc130d56f653dbabb17d66
DhanushiaJ <48893429+DhanushiaJ@users.noreply.github.com>
2019-05-26 11:12:22 +1000

    Update User.java

--- src/main/java/rockets/model/User.java
+++ src/main/java/rockets/model/User.java
@@ -23,6 +23,14 @@ public class User extends Entity {
     }
 
     public void setFirstName(String firstName) {
+        notBlank(firstName, "firstName cannot be null or empty");
+        String regEx ="^[a-zA-Z\\s]+";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m=p.matcher(firstName);
+        if (!m.matches())
+        {
+            throw new IllegalArgumentException("Please using alphabetic characters");
+        }
         this.firstName = firstName;
     }
 
@@ -31,6 +39,14 @@ public class User extends Entity {
     }
 
     public void setLastName(String lastName) {
+        notBlank(lastName, "firstName cannot be null or empty");
+        String regEx ="^[a-zA-Z\\s]+";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m=p.matcher(lastName);
+        if (!m.matches())
+        {
+            throw new IllegalArgumentException("Please using alphabetic characters");
+        }
         this.lastName = lastName;
     }
 
@@ -40,12 +56,29 @@ public class User extends Entity {
 
     public void setEmail(String email) {
         notBlank(email, "email cannot be null or empty");
-        this.email = email;
+        Pattern pattern = Pattern.compile("^.+@.+\\..+$");
+        Matcher matcher = pattern.matcher(email);
+        if(matcher.matches()){
+            this.email = email;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid Email format");
+        }
+
     }
 
     public String getPassword() {
         return password;
     }
+
+    /*public void setPassword(String password) {
+        notBlank(password, "password cannot be null or empty");
+        Pattern numberPattern=Pattern.compile("[0-9]+");
+        Pattern characterPattern=Pattern.compile("[a-zA-Z]+");
+        Matcher numberMatcher=numberPattern.matcher(password);
+        Matcher characterMather=characterPattern.matcher(password);
+        this.password = password;
+    }*/
 
     public void setPassword(String password) {
         notBlank(password, "password cannot be null or empty");
@@ -53,12 +86,26 @@ public class User extends Entity {
         Pattern characterPattern=Pattern.compile("[a-zA-Z]+");
         Matcher numberMatcher=numberPattern.matcher(password);
         Matcher characterMather=characterPattern.matcher(password);
+        if(password.trim().length()<8|!numberMatcher.find()|!characterMather.find())
+        {
+            throw new IllegalArgumentException("Password should more than 8 characters and at least have one character and one number");
+        }
         this.password = password;
     }
 
     // match the given password against user's password and return the result
     public boolean isPasswordMatch(String password) {
         return this.password.equals(password.trim());
+    }
+
+   public boolean checkUnique(String email,User user) {
+        if(user.getEmail().equals(email))
+        {
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 
     @Override
